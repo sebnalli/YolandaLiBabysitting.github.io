fc4797720d5a0f6ad70e30e66ac65b8558953ba1
sebnalli <nallisebastian@gmail.com>
2025-08-10 20:34:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".panel-btn").forEach(button => {
+    button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+});
